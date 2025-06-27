a1a99f5e1e3690792d7e144cdf6b9b1569048d4c
Sigma908-get <lelesungut883@gmail.com>
2025-06-27 19:41:45 +0700

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,12 +2,12 @@ const coinsList = document.getElementById('coins-list');
 const exchangesList = document.getElementById('exchanges-list');
 const nftsList = document.getElementById('nfts-list');
 
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
     const params = new URLSearchParams(window.location.search);
     const query = params.get('query');
-    if(query){
+    if (query) {
         fetchSearchResult(query, [coinsList, exchangesList, nftsList]);
-    }else{
+    } else {
         const searchHeading = document.getElementById('searchHeading');
         const searchContainer = document.querySelector('.search-container');
         searchContainer.innerHTML = '<p style="color: red; text-align: center; margin-bottom: 8px">Nothing To Show...</p>';
@@ -15,14 +15,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
     }
 });
 
-function fetchSearchResult(param, idsToToggle){
-
+function fetchSearchResult(param, idsToToggle) {
     const searchHeading = document.getElementById('searchHeading');
 
-    idsToToggle.forEach(id =>{
+    idsToToggle.forEach(id => {
         const errorElement = document.getElementById(`${id}-error`);
-
-        if(errorElement){
+        if (errorElement) {
             errorElement.style.display = 'none';
         }
         toggleSpinner(id, `${id}-spinner`, true);
@@ -35,17 +33,17 @@ function fetchSearchResult(param, idsToToggle){
     searchHeading.innerText = `Search results for "${param}"`;
 
     const url = `https://api.coingecko.com/api/v3/search?query=${param}`;
-    const options = {method: 'GET', headers: {accept: 'application/json'}};
+    const options = { method: 'GET', headers: { accept: 'application/json' } };
 
     fetch(url, options)
-        .then(response =>{
-            if(!response.ok){
+        .then(response => {
+            if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
             }
             idsToToggle.forEach(id => toggleSpinner(id, `${id}-spinner`, false));
             return response.json();
         })
-        .then(data =>{
+        .then(data => {
             let coins = (data.coins || []).filter(coin => coin.thumb !== "missing_thumb.png");
             let exchanges = (data.exchanges || []).filter(ex => ex.thumb !== "missing_thumb.png");
             let nfts = (data.nfts || []).filter(nf => nf.thumb !== "missing_thumb.png");
@@ -56,7 +54,7 @@ function fetchSearchResult(param, idsToToggle){
 
             let minCount = Math.min(coinsCount, exchangesCount, nftsCount);
 
-            if(coinsCount > 0 && exchangesCount > 0 & nftsCount > 0){
+            if (coinsCount > 0 && exchangesCount > 0 && nftsCount > 0) {
                 coins = coins.slice(0, minCount);
                 exchanges = exchanges.slice(0, minCount);
                 nfts = nfts.slice(0, minCount);
@@ -66,21 +64,21 @@ function fetchSearchResult(param, idsToToggle){
             exchangesResult(exchanges);
             nftsResult(nfts);
 
-            if(coins.length === 0){
+            if (coins.length === 0) {
                 coinsList.innerHTML = '<p style="color: red; text-align: center;">No results found for coins.</p>';
             }
 
-            if(exchanges.length === 0){
+            if (exchanges.length === 0) {
                 exchangesList.innerHTML = '<p style="color: red; text-align: center;">No results found for exchanges.</p>';
             }
 
-            if(nfts.length === 0){
+            if (nfts.length === 0) {
                 nftsList.innerHTML = '<p style="color: red; text-align: center;">No results found for nfts.</p>';
             }
 
         })
-        .catch(error =>{
-            idsToToggle.forEach(id =>{
+        .catch(error => {
+            idsToToggle.forEach(id => {
                 toggleSpinner(id, `${id}-spinner`, false);
                 document.getElementById(`${id}-error`).style.display = 'block';
             });
@@ -88,61 +86,59 @@ function fetchSearchResult(param, idsToToggle){
         });
 }
 
-function coinsResult(coins){
+function coinsResult(coins) {
     coinsList.innerHTML = '';
 
     const table = createTable([
         'Rank', 'Coin'
     ]);
 
-    coins.forEach(coin =>{
+    coins.forEach(coin => {
         const row = document.createElement('tr');
         row.innerHTML = `
            <td>${coin.market_cap_rank}</td>
-            <td class="name-column"><img src="${coin.thumb}" alt="${coin.name}"> ${coin.name} <span>(${coin.symbol.toUpperCase()})</span></td>
+           <td class="name-column"><img src="${coin.thumb}" alt="${coin.name}"> ${coin.name} <span>(${coin.symbol.toUpperCase()})</span></td>
         `;
         table.appendChild(row);
-        row.onclick = () =>{
+        row.onclick = () => {
             window.location.href = `coin.html?coin=${coin.id}`;
         };
     });
     coinsList.appendChild(table);
 }
 
-function exchangesResult(exchanges){
+function exchangesResult(exchanges) {
     exchangesList.innerHTML = '';
 
     const table = createTable([
         'Exchange', 'Market'
     ]);
 
-    exchanges.forEach(ex =>{
+    exchanges.forEach(ex => {
         const row = document.createElement('tr');
         row.innerHTML = `
            <td class="name-column"><img src="${ex.thumb}" alt="${ex.name}"> ${ex.name}</td>
-            <td>${ex.market_type}</td>
+           <td>${ex.market_type}</td>
         `;
         table.appendChild(row);
-        
     });
     exchangesList.appendChild(table);
 }
 
-function nftsResult(nfts){
+function nftsResult(nfts) {
     nftsList.innerHTML = '';
 
     const table = createTable([
         'NFT', 'Symbol'
     ]);
 
-    nfts.forEach(nf =>{
+    nfts.forEach(nf => {
         const row = document.createElement('tr');
         row.innerHTML = `
            <td class="name-column"><img src="${nf.thumb}" alt="${nf.name}"> ${nf.name}</td>
-            <td class="name-column">${nf.symbol}</td>
+           <td class="name-column">${nf.symbol}</td>
         `;
         table.appendChild(row);
-        
     });
     nftsList.appendChild(table);
 }
